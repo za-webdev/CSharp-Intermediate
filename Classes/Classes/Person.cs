11710b8739317cdcf6331e9dbceeb1c1202d2c90
Zoya Ali <33239668+za-webdev@users.noreply.github.com>
2018-11-01 10:55:04 -0400

    first-commit

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Person
+    {
+        
+        public string Name { get; set; }
+        public DateTime Birthdate { get; private set; }
+        private Random rand = new Random();
+
+        public int Age
+        {
+            get
+
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+
+                //return rand.Next(100);
+
+            }
+        }
+        public Person()
+        {
+            
+        }
+
+        public Person(string name,DateTime birthday)
+        {
+            this.Name = name;
+            this.Birthdate = birthday;
+        }
+
+        
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Costumer
+    {
+        static void Main(string[] args)
+        {
+            var costumer = new Person("john", new DateTime(1991, 1, 19));
+
+           
+            Console.WriteLine("My name is {0} and i am {1} years old. ",costumer.Age,costumer.Name);
+
+
+            //costumer.Age = 3;
+            //costumer.Name = "Daffy";
+            //Console.WriteLine("Hi my name is {0} and i am {1} year old", costumer.Name, costumer.Age);
+            //costumer.GettingOld(new Person(10));
+
+            
+            //Console.WriteLine("now my age is {0}",costumer.Age);
+        }
+    }
+}
